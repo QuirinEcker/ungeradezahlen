c88ae9e3c638c43510df4e5d26c68b30170e72ee
QuirinEcker <quirin.ecker@bajupa.com>
2017-11-18 16:07:24 +0100

    finish code

--- src/main/java/Ecker_UngeradeZahlen.java
+++ src/main/java/Ecker_UngeradeZahlen.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Ecker_UngeradeZahlen {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input;
+        int sum = 0;
+
+        System.out.print("Geben Sie die maximale ungerade Zahl ein :");
+        input = scanner.nextInt();
+        System.out.print("Ungerade Zahlen bis 20: ");
+
+        for (int i = 1 ; i < input; i = i + 2){
+            System.out.print(i + ", ");
+            sum = sum + i;
+        }
+        System.out.println();
+        System.out.println("Summe der Zahlen: " + sum);
+    }
 }
